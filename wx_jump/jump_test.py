e0ae283b14451045dbd517e1001d5b664da91ad6
nicoliyun <24080559@qq.com>
2018-01-26 13:45:38 +0800

    文件2重新提交。

--- wx_jump/jump_test.py
+++ wx_jump/jump_test.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # author:liyun
-
 import time
 import PIL
 import numpy
 import matplotlib.pyplot as plt
 import os
+# import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
-
 # 是否需要更新截图的开关
+
 need_update = True
 
 def get_screen_image():
@@ -19,20 +19,22 @@ def get_screen_image():
     os.system('adb pull /sdcard/wechat_jump.png')
     # 返回图像数据
     return numpy.array(PIL.Image.open('wechat_jump.png'))
-
 def jump_to_next(point1, point2):
     # 分别获取第一下点击鼠标和第二下点击鼠标的坐标值
     x1, y1 = point1
     x2, y2 = point2
     # 计算第一下点击鼠标和第二下点击鼠标之间的像素距离
     distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+    print x1,x2,y1,y2,distance
     # 模拟按压屏幕，前两个数字是点击屏幕起始坐标，三四个数字是点击屏幕的终止坐标，最后一个数字是按压时间，需要自己测试
-    os.system('adb shell input swipe 320 410 320 410 {}'.format(int(distance*1.35)))
+    os.system('adb shell input swipe 320 410 320 410'+str(distance*1.35))
 
 def click(event, coor=[]):
     global need_update
+    print "click----" + str(need_update)
     # 获取鼠标点击点的坐标值，以元组形式添加至坐标列表
     coor.append((event.xdata, event.ydata))
+    print len(coor)
     # 直到点了两下屏幕，开始调用鼠标点击函数
     if len(coor)==2:
         jump_to_next(coor.pop(), coor.pop())
@@ -41,17 +43,18 @@ def click(event, coor=[]):
 
 def update_screen(frame):
     global need_update
+    print "update----"+str(need_update)
     if need_update:
-        time.sleep(2)
+        time.sleep(3)
         #更新截图
         myimage.set_array(get_screen_image())
         #关闭更新截图开关
         need_update = False
     #返回元组数据
     return myimage,
-
 #以指定尺寸创建一块画布
-figure = plt.figure(figsize=(6, 8))
+# figure = plt.figure(figsize=(6, 8))
+figure = plt.figure()
 #用得到的图像数据在画布上复制出截图
 myimage = plt.imshow(get_screen_image(), animated=True)
 #使画布上的画更加紧凑
@@ -59,6 +62,6 @@ myimage = plt.imshow(get_screen_image(), animated=True)
 #将鼠标点击事件与click函数绑定
 figure.canvas.mpl_connect('button_press_event',click)
 #更新截图
-update = FuncAnimation(figure, update_screen, interval=50, blit=True)
+ani = FuncAnimation(figure, update_screen, interval=50, blit=True)
 #显示图片
 plt.show()
