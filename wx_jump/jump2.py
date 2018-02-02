c6ab91ca34bb1ab107fc5f0c09cfff9686e5df5e
nicoliyun <24080559@qq.com>
2018-02-02 08:40:24 +0800

    文件2重新提交。

---
+++ wx_jump/jump2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author:liyun
+
+__author__ = 'wechat'
+import os
+import PIL,numpy
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import time
+
+need_update = True
+
+def get_screen_image():
+    os.system('adb shell screencap -p /sdcard/screen.png')#获取当前界面的手机截图
+    os.system('adb pull /sdcard/screen.png')#下载当前这个截图到当前电脑当前文件夹下
+    return numpy.array(PIL.Image.open('screen.png'))
+
+def jump_to_next(point1, point2):#计算炫的长度
+    x1, y1 = point1; x2, y2 = point2
+    distance = ((x2-x1)**2 + (y2-y1)**2)**0.5
+    os.system('adb shell input swipe 320 410 320 410 {}'.format(int(distance*1.35)))
+
+def on_calck(event, coor=[]):#绑定的鼠标单击事件
+    global need_update
+    coor.append((event.xdata, event.ydata))
+    if len(coor) == 2:
+        jump_to_next(coor.pop(), coor.pop())
+    need_update = True
+
+def update_screen(frame):#更新图片 /从画图片
+    global need_update
+    if need_update:
+        time.sleep(1)
+        axes_image.set_array(get_screen_image())
+        need_update = False
+    return axes_image,
+
+figure = plt.figure()#创建一个空白的图片对象/创建一张图片
+axes_image = plt.imshow(get_screen_image(), animated=True)#把获取的图片话在坐标轴上面
+figure.canvas.mpl_connect('button_press_event', on_calck)
+ani = FuncAnimation(figure, update_screen, interval=50, blit=True)
+plt.show()
--- wx_jump/test.py
+++ wx_jump/test.py
@@ -6,3 +6,13 @@ astr = 'adb shell input swipe 320 410 320 410 {}'.format(int(1.35))
 cmd = 'adb shell input swipe 320 410 320 410 ' + str(1.35)
 print astr
 print cmd
+
+keym = {"a":"123","b":"345","c":"567"}
+print keym["a"]
+
+
+bizContent = {"merchantTradeNo":"12356564","totalAmount":"5200.00","productCode":"30100501","userId":"${userId}","merchantExtParams":"rereerer"}
+
+bizContents = {"merchantTradeNo":"TEST-20180127095408416-1VUaVVgV","productCode":"30100501","totalAmount":999,"userId":3172}
+
+
